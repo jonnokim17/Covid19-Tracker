17ff408e40f7a067cf4a88d696f52a7121931f6a
Jonathan Kim <jonathankim@Jonathans-MacBook-Pro.local>
2020-03-17 13:46:16 -0500

    added project files

---
+++ Covid19 Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19 Tracker
+//
+//  Created by Jonathan Kim on 3/17/20.
+//  Copyright © 2020 jonno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Covid19 TrackerTests/Covid19_TrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Covid19_TrackerTests.swift
+//  Covid19 TrackerTests
+//
+//  Created by Jonathan Kim on 3/17/20.
+//  Copyright © 2020 jonno. All rights reserved.
+//
+
+import XCTest
+@testable import Covid19_Tracker
+
+class Covid19_TrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
